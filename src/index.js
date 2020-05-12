820832b9b1c523a2115755730c423d4a7e973f87
Olia_Antipenko <olia95shash@gmail.com>
2020-05-12 21:29:50 +0300

    module-10

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import dishes from './menu.json';
+import menuItemTemplate from './list-item.hbs';
+import './styles.css';
+
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+  input: document.querySelector('.js-switch-input'),
+  body: document.querySelector('body'),
+};
+
+function buildItem(dishes) {
+  const markup = dishes.map(dish => menuItemTemplate(dish)).join('');
+  refs.menuList.insertAdjacentHTML('beforeend', markup);
+}
+
+buildItem(dishes);
+
+// THEME
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+document.addEventListener('DOMContentLoaded', getThemeFromLocalStorage);
+
+function getThemeFromLocalStorage() {
+  if (JSON.parse(localStorage.getItem('theme')) === Theme.LIGHT) {
+    setLightTheme();
+  } else if (JSON.parse(localStorage.getItem('theme')) === Theme.DARK) {
+    setDarkTheme();
+    refs.input.checked = true;
+  }
+}
+
+refs.input.addEventListener('change', changeTheme);
+
+function changeTheme(e) {
+  if (!e.target.checked) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+}
+
+function setDarkTheme() {
+  refs.body.classList.remove(Theme.LIGHT);
+  refs.body.classList.add(Theme.DARK);
+
+  localStorage.removeItem('theme', JSON.stringify(Theme.LIGHT));
+  localStorage.setItem('theme', JSON.stringify(Theme.DARK));
+}
+
+function setLightTheme() {
+  refs.body.classList.remove(Theme.DARK);
+  refs.body.classList.add(Theme.LIGHT);
+
+  localStorage.removeItem('theme', JSON.stringify(Theme.DARK));
+  localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+}
